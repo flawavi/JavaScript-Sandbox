2a2c816dacb33010a1bbaa107cea60bbe67da807
Fletcher Watson <fbwthe6@gmail.com>
2017-01-25 12:22:44 -0600

    love cures hate.

---
+++ main.js
@@ -0,0 +1,31 @@
+let love = {
+  status: "pure",
+  trust: true,
+  honesty: true,
+  cooperate: true,
+  communication: "strong",
+  fuck: (person1, person2) => `${person1} and ${person2} are making sweet, sweet love to each other. Their relationship is ${love.status}`
+}
+console.log(love, love.fuck("X", "Y"))
+
+let hate = {
+  status: "toxic",
+  trust: false,
+  honesty: false,
+  cooperate: false,
+  communication: "poor",
+  fuck: (person1, person2) => `${person1} hatefucks ${person2} frequently and they don't even know it but because their trust is ${hate.trust} and their communication is ${hate.communication}, they are growing apart and it's fucking ${hate.status}.`
+}
+
+console.log(hate, hate.fuck("X", "Y"))
+
+let cure = Object.assign({
+  status: "toxic",
+  trust: false,
+  honesty: false,
+  cooperate: false,
+  communication: "poor",
+  fuck: (person1, person2) => `${person1} hatefucks ${person2} frequently and they don't even know it but because their trust is ${hate.trust} and their communication is ${hate.communication}, they are growing apart and it's fucking ${hate.status}.`
+},love)
+
+console.log(cure, hate.fuck("X", "Y"))
