8abed99cafea2a79c2f650810bcc129005bd05cc
Fletcher Watson <fbwthe6@gmail.com>
2017-01-26 16:42:17 -0600

    booleans...

--- Filter.js
+++ Filter.js
@@ -1,9 +1,9 @@
 let numData = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-let stringData = ['a', 'ab', 'abc', 'abcd', 'abcde', 'abcdef']
+let stringData = ['a', 'ab', 'abc', 'abcd', true, 'abcdef']
 
 let numDataFiltered = numData.filter(num => num >= 10)
 let stringDataFiltered = stringData.filter(str => {
-  if(str === 'ab') return str
+  if(str === 'ab' || str === true) return str
 })
 
 
